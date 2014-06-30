c164ec48d7ae0a3fdd6ec03dbc63ed914d39554e
gefry52 <gefry52@yandex.ru>
2014-06-30 05:47:55 +0300

    29.06.14 STOP

---
+++ Pomodoro/Controls/TaskViewControl.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pomodoro.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskViewControl.xaml
+    /// </summary>
+    public partial class TaskViewControl : UserControl
+    {
+
+        public static readonly DependencyProperty TaskTitleProperty = DependencyProperty.Register(
+                   "TaskTitle",
+                   typeof(string),
+                   typeof(TaskViewControl),
+                   new PropertyMetadata("Title is empty", new PropertyChangedCallback(OnChangeTitle))
+                   );
+
+        public static readonly DependencyProperty TaskDescriptionProperty = DependencyProperty.Register(
+                    "TaskDescription",
+                    typeof(string),
+                    typeof(TaskViewControl),
+                    new PropertyMetadata("Title is empty", new PropertyChangedCallback(OnChangeDescription))
+                    );
+
+        public static readonly DependencyProperty IsEditableProperty = DependencyProperty.Register(
+                    "IsEditable",
+                    typeof(bool),
+                    typeof(TaskViewControl),
+                    new PropertyMetadata(false)
+                    );
+
+        public static readonly DependencyProperty TaskPomodorosSpentProperty = DependencyProperty.Register(
+                    "TaskPomodorosSpent",
+                    typeof(string),
+                    typeof(TaskViewControl),
+                    new PropertyMetadata("0",new PropertyChangedCallback(OnChangePomodorosSpent))
+                    );
+
+        public static readonly DependencyProperty TaskPriorytiItemsProperty = DependencyProperty.Register(
+                    "TaskPriorytiItems",
+                    typeof(ObservableCollection<string>),
+                    typeof(TaskViewControl),
+                    new PropertyMetadata(null, new PropertyChangedCallback(OnChangePriorytiItems))
+                    );
+
+        public static readonly DependencyProperty SelectedPriorytiItemIndexProperty = DependencyProperty.Register(
+                    "SelectedPriorytiIndex", 
+                    typeof(Int32), 
+                    typeof(TaskViewControl),
+                    new PropertyMetadata(0, new PropertyChangedCallback(OnChangeSelectedIndex)) 
+                    );
+
+        public static readonly DependencyProperty TaskStateProperty = DependencyProperty.Register(
+                    "TaskState",
+                    typeof(bool),
+                    typeof(TaskViewControl),
+                    new PropertyMetadata(false, new PropertyChangedCallback(OnChangeState))
+                    );
+
+        public String TaskTitle
+        {
+            get { return GetValue(TaskTitleProperty) as String;}
+            set { this.SetValue(TaskTitleProperty, value); }
+        }
+
+        public String TaskDescription
+        {
+            get { return GetValue(TaskDescriptionProperty) as String; }
+            set { this.SetValue(TaskDescriptionProperty, value); }
+        }
+
+        public bool IsEditable 
+        {
+            get{ return (bool) GetValue(IsEditableProperty);}
+            set { this.SetValue(IsEditableProperty, value); }
+        }
+
+        public String TaskPomodorosSpent 
+        {
+            get { return GetValue(TaskPomodorosSpentProperty) as String; }
+            set { this.SetValue(TaskPomodorosSpentProperty, value); }
+        }
+
+        public ObservableCollection<string> TaskPriorytiItems 
+        {
+            get { return GetValue(TaskPriorytiItemsProperty) as ObservableCollection<string>; }
+            set { this.SetValue(TaskPriorytiItemsProperty, (ObservableCollection<string>)value); }
+        }
+
+        public Int32 SelectedPriorytiIndex 
+        {
+            get { return (Int32) GetValue(SelectedPriorytiItemIndexProperty); }
+            set { this.SetValue(SelectedPriorytiItemIndexProperty, value); }
+        }
+
+        public bool TaskState 
+        {
+
+            get { return (bool)GetValue(TaskStateProperty); }
+            set { this.SetValue(TaskStateProperty, value); }
+        } 
+
+
+
+        private static void OnChangeTitle(DependencyObject sender, DependencyPropertyChangedEventArgs args) 
+        {
+            TaskViewControl _control = (TaskViewControl)sender;
+            _control.TitleTextBox.Text = (string)args.NewValue;
+        }
+
+
+        private static void OnChangeDescription(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            TaskViewControl _control = (TaskViewControl)sender;
+            _control.DescriptionTextBox.Text = (string)args.NewValue;
+        }
+
+        private static void OnChangePriorytiItems(DependencyObject sender, DependencyPropertyChangedEventArgs args) 
+        {
+            TaskViewControl _control = (TaskViewControl)sender;
+            foreach (string item in (ObservableCollection<string>)args.NewValue) 
+            {
+                _control.PriorytiComboBox.Items.Add(item);
+            }
+        }
+        private static void OnChangeSelectedIndex(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            TaskViewControl _control = (TaskViewControl)sender;
+            _control.PriorytiComboBox.SelectedIndex = (int)args.NewValue;
+
+        }
+
+        private static void OnChangePomodorosSpent(DependencyObject sender, DependencyPropertyChangedEventArgs args) 
+        {
+            TaskViewControl _control = (TaskViewControl)sender;
+            _control.PomodoroSpentLabel.Content = (string)args.NewValue;
+        }
+
+        private static void OnChangeIsEditable(DependencyObject sender, DependencyPropertyChangedEventArgs args) 
+        {
+            TaskViewControl _control = (TaskViewControl)sender;
+            bool arg = (bool)args.NewValue;
+            _control.DescriptionTextBox.IsEnabled = arg;
+            _control.TitleTextBox.IsEnabled = arg;
+            _control.PriorytiComboBox.IsEnabled = arg;
+            _control.StateCheckBox.IsEnabled = arg;
+        }
+
+        private static void OnChangeState(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            TaskViewControl _control = (TaskViewControl)sender;
+            _control.StateCheckBox.IsChecked = (bool) args.NewValue;
+        }
+
+        public TaskViewControl()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
--- Pomodoro/Pages/HomeViewModel.cs
+++ Pomodoro/Pages/HomeViewModel.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using CountdownTimer;
 using System.Windows.Input;
 using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace Pomodoro.Pages
 {
@@ -26,12 +27,85 @@ namespace Pomodoro.Pages
             {
                 _currentTask=value;
                 OnPropertyChanged("CurrentTask");
+                OnPropertyChanged("CurrentTaskTitle");
+                OnPropertyChanged("CurrentTaskDescription");
+                OnPropertyChanged("CurrentTaskPriorytiIndex");
+                OnPropertyChanged("CurrentTaskPomodorosSpent");
+                OnPropertyChanged("CurrentTaskState");
+            }
+        }
+
+        public String CurrentTaskTitle 
+        {
+            get { return _currentTask.Title; }
+            set {
+                    _currentTask.Title = value;
+                    OnPropertyChanged("CurrentTask");
+                }
+        }
+
+        public String CurrentTaskDescription
+        {
+            get { return _currentTask.Description; }
+            set
+            {
+                _currentTask.Description = value;
+                OnPropertyChanged("CurrentTask");
+            }
+        }
+
+        public ObservableCollection<string> TaskPriorytiItem 
+        {
+            get 
+            {
+                ObservableCollection<string> _collection = new ObservableCollection<string>();
+               foreach( var str in Enum.GetValues(typeof(Model.Priority)))
+                {
+                    _collection.Add(str.ToString());
+                }
+               return _collection; 
+            }
+            set {}
+        }
+
+        public int CurrentTaskPriorytiIndex 
+        {
+            get { return (int)_currentTask.Priority; }
+            set 
+            { 
+                _currentTask.Priority = (Model.Priority)value; 
+                OnPropertyChanged("CurrentTask"); 
+            }
+        }
+
+        public int CurrentTaskPomodorosSpent 
+        {
+            get { return _currentTask.CountPomodoroUnit; }
+            set 
+            {
+                _currentTask.CountPomodoroUnit = value;
+                OnPropertyChanged("CurrentTask");
+            }
+        }
+
+        public bool CurrentTaskState 
+        {
+            get
+            {
+                if (_currentTask.State == Model.State.done) return true;
+                else return false;
+            }
+            set
+            {
+                if (value) _currentTask.State = Model.State.done;
+                else _currentTask.State = Model.State.open;
+                OnPropertyChanged("CurrentTask");
             }
         }
 
         public HomeViewModel() 
         {
-            _currentTask = new Model.TaskModel("bbbb");
+            
         }
 
         public void OnPropertyChanged(string name)
@@ -59,8 +133,9 @@ namespace Pomodoro.Pages
 
         private void ExecuteSetCurrentTask()
         {
-            
-            this.CurrentTask = new  Model.TaskModel( "huyna"); 
+            int i = 1;
+            CurrentTask = new Model.TaskModel("huyna", Model.Priority.low, "asdasd" + i) { CountPomodoroUnit=3,State = Model.State.done};
+            i++;
         }
 
     }
